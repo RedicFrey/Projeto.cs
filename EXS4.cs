7aab4f1a7cc0ebdaadc4fcf10dfbb6f07b21d510
RedicFrey <danielcorradilavarini@gmail.com>
2025-06-09 19:48:56 -0300

    EXS4.cs

--- EXS4.cs
+++ EXS4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,72 @@ namespace Projeto.cs
         public EXS4()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int valor = 0;
+
+
+            if (checkedListBox1.GetItemChecked(0))
+            {
+                valor += 1;
+            }
+                
+
+
+            if (checkedListBox1.GetItemChecked(1))
+            {
+                valor += 2;
+            }
+                
+
+
+            if (checkedListBox1.GetItemChecked(2))
+            {
+                valor += 3;
+            }
+                
+        
+            
+            if (checkedListBox1.GetItemChecked(3))
+            {
+                valor += 4;
+            }
+
+            if (checkedListBox2.GetItemChecked(0))
+            {
+                valor += 5;
+            }
+
+
+
+            if (checkedListBox2.GetItemChecked(1))
+            {
+                valor += 6;
+            }
+
+
+
+            if (checkedListBox2.GetItemChecked(2))
+            {
+                valor += 7;
+            }
+
+
+
+            if (checkedListBox2.GetItemChecked(3))
+            {
+                valor += 8;
+            }
+
+            txt.Text = "Valor total: " + valor.ToString();
+
         }
     }
 }
